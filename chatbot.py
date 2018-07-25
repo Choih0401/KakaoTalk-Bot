4cf9225391ffba55cc716ee3bc65153a57bbb4a0
a-8-a <cogksmf02@naver.com>
2018-07-25 19:17:14 +0900

    add .py

---
+++ chatbot.py
@@ -0,0 +1,30 @@
+from flask import Flask, request, jsonify
+from random import random
+app = Flask(__name__)
+
+@app.route('/keyboard')
+def keyboard():
+	return {"type" : "buttons","buttons" : ["시작하기", "도움말"]}
+
+@app.route('/message', methods=['POST'])
+def message():
+	dataReceive = request.get_json()
+	content = dataReceive['content']
+
+	if (content == '랜덤'):
+		reqdata = {
+			"message" : {
+			"text" : str(random.randrange(1,27))
+			}
+		}
+	elif (content == '자리 배치'):
+		reqdata = {
+			"message" : {
+			"text" : "개발중인 기능이거나 잘못된 입력입니다TnT"
+			}
+		}
+
+	return reqdata
+
+if __name__ == '__main__':
+	app.run(host="0.0.0.0", port=5000)
