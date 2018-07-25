754d7c5eebbe1eadf89bfd18d2f293b03549432e
a-8-a <cogksmf02@naver.com>
2018-07-26 01:13:41 +0900

    add setSeat

--- chatbot.py
+++ chatbot.py
@@ -1,30 +1,58 @@
-﻿from flask import Flask, request, jsonify
-from random import random
+﻿import os
+from flask import Flask, request, jsonify
+from random import random, randint
 app = Flask(__name__)
 
+def setSeat():
+	num = [i+1 for i in range(26)]
+	idx = 26
+	s = ""
+	for i in range(5):
+		for j in range(6):
+			s += str(num.pop(random.randrange(idx)))+' '
+			idx -= 1
+			print(idx)
+			if not idx:
+				break
+		s += '\n'
+	return s
+
 @app.route('/keyboard')
-def keyboard():
-	return jsonify({"type" : "buttons","buttons" : ["시작하기", "도움말"]})
+def Keyboard():
+   reqdata = {
+      "type" : "buttons",
+      "buttons" : ["랜덤", "자리 배치"]
+   }
+   return jsonify(reqdata)
 
 @app.route('/message', methods=['POST'])
-def message():
-	dataReceive = request.get_json()
-	content = dataReceive['content']
+def Message():
+   dataReceive = request.get_json()
+   content = dataReceive['content']
+   a = randint(1,26)
 
-	if (content == '랜덤'):
-		reqdata = {
-			"message" : {
-			"text" : str(random.randrange(1,27))
-			}
-		}
-	elif (content == '자리 배치'):
-		reqdata = {
-			"message" : {
-			"text" : "개발중인 기능이거나 잘못된 입력입니다TnT"
-			}
-		}
+   if (content == '랜덤'):
+        reqdata = {
+            "message" : {
+                "text" : str(a)
+            },
+            "keyboard": {
+                "type" : "buttons",
+                "buttons" : ["랜덤", "자리 배치"]
+            }
+        }
+   elif (content == '자리 배치'):
+        reqdata = {
+            "message" : {
+                "text" : setSeat()
+            },
+            "keyboard": {
+                "type" : "buttons",
+                "buttons" : ["랜덤", "자리 배치"]
+            }
+        }
 
-	return jsonify(reqdata)
+   return jsonify(reqdata)
 
 if __name__ == '__main__':
-	app.run(host="0.0.0.0", port=5000)
+   app.run(host="0.0.0.0", port=3389)
